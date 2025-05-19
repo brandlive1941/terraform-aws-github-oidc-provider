ac1b846d830fe2a6d9c70bb41442d7dc47b3f27d
drewmercer-bl <drew.mercer@brandlive.com>
2025-05-19 13:49:48 -0700

    feat(main.tf): thumprint as list

--- variables.tf
+++ variables.tf
@@ -22,8 +22,8 @@ variable "create_oidc_role" {
 # See: https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
 variable "github_thumbprint" {
   description = "GitHub OpenID TLS certificate thumbprint."
-  type        = string
-  default     = "6938fd4d98bab03faadb97b34396831e3780aea1"
+  type        = list(string)
+  default     = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 variable "repositories" {
